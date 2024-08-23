1bd2e328c7ef3c63d871e8808f990e2df39a3493
ousleboss <67784841+ousleboss@users.noreply.github.com>
2024-08-23 23:26:08 +0100

    Add files via upload

---
+++ my-website.bash
@@ -0,0 +1,16 @@
+/my-website
+    /css
+        style.css
+    /js
+        main.js
+    /images
+    /php
+        contact.php
+    index.html
+    conseil.html
+    formation.html
+    vente-donnees.html
+    blog.html
+    a-propos.html
+    contact.html
+    
---
+++ script.js
@@ -0,0 +1,5 @@
+// Code JavaScript pour les interactions si nécessaire
+
+document.querySelector('.cta-btn').addEventListener('click', function () {
+  alert('Demande de consultation envoyée !');
+});
